018ef0a7092d1b3a879b8cac7bc10b6a4eb6b60e
Senimtra <senimtra@hotmail.com>
2021-07-01 21:12:41 +0200

    solve iteration 2: the character class

--- main.js
+++ main.js
@@ -29,3 +29,26 @@ function drawEverything() {
 }
 
 drawEverything();
+
+// ######################################
+// ## Iteration 2: The Character Class ##
+// ######################################
+
+class Character {
+   constructor(col, row) {
+      this.col = col;
+      this.row = row;
+   }
+   moveUp = () => this.row--;
+   moveRight = () => this.col++;
+   moveDown = () => this.row++;
+   moveLeft = () => this.col--;
+}
+
+const player = new Character(0, 0); // (0,0) = Initial position
+
+player.moveDown(); // Increase by 1 the value of player.row
+player.moveDown(); // Increase by 1 the value of player.row
+player.moveRight(); // Increase by 1 the value of player.col
+
+console.log(player.col, player.row); // => 1,2
